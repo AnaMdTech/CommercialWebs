11c9142175c86d5ede9bddec37162c4dc3421256
Anamd-hub <anasmdl120@gmail.com>
2024-11-05 02:58:40 +0300

    Complete mobile navigation header menu implementation

--- Responsive-Burger-Website/assets/js/main.js
+++ Responsive-Burger-Website/assets/js/main.js
@@ -1,16 +1,36 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
+/* MENU SHOW */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+/* MENU HIDDEN */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
 
+const linkAction = () => {
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((link) => link.addEventListener("click", linkAction));
 
 /*=============== ADD SHADOW HEADER ===============*/
 
-
-/*=============== SHOW SCROLL UP ===============*/ 
-
+/*=============== SHOW SCROLL UP ===============*/
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
